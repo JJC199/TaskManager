cbd45d0bc9ff47fc85e667ae305946398c5b9e4c
Ricardo Sikaffy <rsikaffy@gmail.com>
2016-06-22 21:35:08 -0600

    Added Comments

--- TheDadTaskManager/taskcollection.cpp
+++ TheDadTaskManager/taskcollection.cpp
@@ -2,12 +2,15 @@
 
 taskcollection::taskcollection()
 {
+    //Fill the array with samples of tasks and sets them inactive.
+    //By setting them inactive, they can be compared to add a new one.
     for(int x = 0; x < 15; x++){
         tasklist[x] = Task();
         tasklist[x].setInactive();
     }
 }
 
+//Returns task at the chosen location in the array.
 Task taskcollection::getTaskAt(int position){
     if(position >= 0 && position < 15)
         for(int x = 0; x < 15; x++)
@@ -16,6 +19,7 @@ Task taskcollection::getTaskAt(int position){
     return tasklist[0];
 }
 
+//Removes task at the chosen location.
 void taskcollection::removeTaskAt(int position){
     if(position > 0 && position <= 15)
         for(int x = 0; x < 15; x++)
@@ -24,8 +28,10 @@ void taskcollection::removeTaskAt(int position){
             }
 }
 
+//Adds a new task in the first available location in the array.
 void taskcollection::addTask(Task addition){
     for(int x = 0; x < 15; x++)
+        //If the task in that location is not active (deleted) it is replaced by the new task.
         if(!tasklist[x].ifActive())
             tasklist[x] = addition;
 }

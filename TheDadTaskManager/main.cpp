7c7a1fa3cb2664499fc865f5694989f575736ca0
JJC199 <juancarranza96@hotmail.com>
2016-06-26 16:36:05 -0600

    add obl

--- TheDadTaskManager/main.cpp
+++ TheDadTaskManager/main.cpp
@@ -3,8 +3,13 @@
 
 int main(int argc, char *argv[])
 {
+    home_task m("HW", "LAUNDRY");
+    school_task c("QUEMISTRY","INVESTIGATUION");
     QApplication a(argc, argv);
     MainWindow w;
+    newtask k;
+    k.newtask(m);
+    k.newtask(c);
     w.show();
 
     return a.exec();
